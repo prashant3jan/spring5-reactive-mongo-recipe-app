4f51ada1a65aa5aab3854cf69ce999b67001c830
prashant3jan <prashant3jan@gmail.com>
2022-11-02 11:38:43 +0530

    webflux exception handling

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -24,8 +24,6 @@ public class RecipeController {
     private final RecipeService recipeService;
     private WebDataBinder webDataBinder;
 
-    private String value;
-
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
