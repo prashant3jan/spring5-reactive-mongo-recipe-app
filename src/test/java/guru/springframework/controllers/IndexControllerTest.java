b15d60445421fccef280db51d5563cfd2a7cb8c3
Prashant Singh <prashant3jan@gmail.com>
2023-06-25 15:39:16 +0530

    adding test file and build.gradle

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -3,75 +3,105 @@ package guru.springframework.controllers;
 import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import org.junit.Before;
-import org.junit.Test;
-import org.mockito.ArgumentCaptor;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.ui.Model;
+import org.springframework.web.reactive.function.server.RouterFunction;
 import reactor.core.publisher.Flux;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import java.util.*;
+
+
 
 /**
  * Created by jt on 6/17/17.
  */
+
+@ExtendWith(MockitoExtension.class)
+@WebFluxTest(controllers = {IndexController.class})
+@Import(RecipeService.class)
 public class IndexControllerTest {
-    @Mock
+
+
+    @MockBean
     RecipeService recipeService;
+
+
+    private WebTestClient webTestClient;
 
     @Mock
     Model model;
 
     IndexController controller;
 
-    @Before
+    @BeforeEach
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         controller = new IndexController(recipeService);
-    }
-
-    @Test
-    public void testMockMVC() throws Exception {
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-
-        when(recipeService.getRecipes()).thenReturn(Flux.empty());
-
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("index"));
+//        RouterFunction<?> routerFunction = controller.routes(recipeService);
+        webTestClient = WebTestClient.bindToController(controller).build();
     }
 
     @Test
     public void getIndexPage() throws Exception {
         //given
-        Set<Recipe> recipes = new HashSet<>();
-        recipes.add(new Recipe());
+        List<Recipe> recipes = new ArrayList<>();
+        Recipe recipe1 = new Recipe();
+        recipe1.setId("1");
+        recipes.add(recipe1);
 
-        Recipe recipe = new Recipe();
-        recipe.setId("1");
-        recipes.add(recipe);
-        when(recipeService.getRecipes()).thenReturn(Flux.fromIterable(recipes));
-        ArgumentCaptor<List<Recipe>> argumentCaptor = ArgumentCaptor.forClass(List.class);
+        Recipe recipe2 = new Recipe();
+        recipe2.setId("1");
+        recipes.add(recipe2);
 
-        //when
-        String viewName = controller.getIndexPage(model);
+        Flux<Recipe> recipeFlux = Flux.fromIterable(recipes);
+        Mockito.when(recipeService.getRecipes()).thenReturn(recipeFlux);
 
-        //then
-        assertEquals("index", viewName);
-        verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
-        List<Recipe> setInController = argumentCaptor.getValue();
-        assertEquals(2, setInController.size());
+
+        webTestClient.get().uri("/")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk();
+//                .expectBodyList(Recipe.class)
+//                .returnResult()
+//                .getResponseBody();
+//        Assertions.assertEquals(2, actual.size());
+
+
+
+
+//        ArgumentCaptor<List<Recipe>> argumentCaptor = ArgumentCaptor.forClass(List.class);
+//
+//        when
+//        String viewName = controller.getIndexPage(model);
+//        System.out.println("viewName"+viewName);
+
+//        //then
+//        assertEquals("index", viewName);
+//        Mockito.verify(recipeService, times(1)).getRecipes();
+//        verify(model, times(1)).addAttribute(eq("recipes"), model.getAttribute("recipes"));
+//        List<Recipe> setInController = (List<Recipe>) model.getAttribute("recipes");
+//        assertEquals(2, recipes.size());
     }
 
 }
